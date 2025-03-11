db0ea0bb91f5134725196523f38c3dfed8e49a9e
Stephano Telolahy <stephano.telolahy@gmail.com>
2025-03-11 07:58:06 +0100

    wip

---
+++ OverlayDemo/OverlayDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  OverlayDemoApp.swift
+//  OverlayDemo
+//
+//  Created by Hugues Stéphano TELOLAHY on 10/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct OverlayDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
